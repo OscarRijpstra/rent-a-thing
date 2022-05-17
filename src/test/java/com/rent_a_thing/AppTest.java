d78342d0ff50c9913c54b8f056baeaf9b052de3c
Oscar Rijpstra <oscar.rijpstra@icloud.com>
2022-05-17 16:47:58 +0200

    init

---
+++ src/test/java/com/rent_a_thing/AppTest.java
@@ -0,0 +1,12 @@
+package com.rent_a_thing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
